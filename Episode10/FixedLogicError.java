56e9356ac986681e98c67a07b19e9e0ca6d2687a
Ho1yShif <shifraisaacs@gmail.com>
2023-01-26 18:27:10 -0500

    Corrected issue in FixedLogicError

--- Episode10/FixedLogicError.java
+++ Episode10/FixedLogicError.java
@@ -8,7 +8,7 @@ public class FixedLogicError {
 		} else if (order.status == "cancelled") {
 			order.totalCost = 0;
 		} else if (order.status == "returned") {
-			order.totalCost(order.getTotalCost() - shippingCost);
+			order.totalCost = order.totalCost - shippingCost;
 		} else {
 			System.out.println("Other status found.");
 		}
