799f65b7475c3d2016c3ee7c2d27377abd2ecd82
Ho1yShif <shifraisaacs@gmail.com>
2023-01-26 22:09:47 -0500

    Implemented Logic Error as function

--- Episode10/FixingLogicError.java
+++ Episode10/FixingLogicError.java
@@ -1,8 +1,8 @@
 package Episode10;
 
-public class BrokenLogicError {
-	// Logic Error: Missing functionality for returned orders
+public class FixingLogicError {
 	public static void processOrder(int productCost, String orderStatus) {
+		// Added functionality for return orders as well as a catch-all else statement
 		double shippingCost = 10.50;
 		double totalCost = 5.00;
 		if (orderStatus == "shipped") {
@@ -11,7 +11,12 @@ public class BrokenLogicError {
 		} else if (orderStatus == "cancelled") {
 			totalCost = 0;
 			System.out.println("The total cost of your cancelled item is now $0.");
-		} 
+		} else if (orderStatus == "returned") {
+			totalCost = totalCost - shippingCost;
+			System.out.println("The total cost of your returned item is $" + totalCost + ".");
+		} else {
+			System.out.println("Unsupported order status.");
+		}
 	}
 
 	public static void main(String[] args) {
