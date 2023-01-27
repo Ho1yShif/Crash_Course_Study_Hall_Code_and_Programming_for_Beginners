3796e6bd7495a8cfc74c7f57ea1706b82afd91bb
Ho1yShif <shifraisaacs@gmail.com>
2023-01-27 12:57:16 -0500

    Add code for fixed runtime exception

---
+++ Episode10/FixedRuntimeException.java
@@ -0,0 +1,9 @@
+package Episode10;
+
+public class FixedRuntimeException {
+	public static void main(String[] args) {
+		String name = "Bob";
+		char c = name.charAt(2);
+		System.out.print(c);
+	}
+}
