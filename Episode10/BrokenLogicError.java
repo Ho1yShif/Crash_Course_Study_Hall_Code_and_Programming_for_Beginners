d504b91a2665c6efdfa2d5c5111625268109c141
Ho1yShif <shifraisaacs@gmail.com>
2023-02-04 20:25:55 -0500

    Added return statements

--- Episode10/BrokenLogicError.java
+++ Episode10/BrokenLogicError.java
@@ -2,16 +2,20 @@ package Episode10;
 
 public class BrokenLogicError {
 	// Logic Error: Missing functionality for returned orders
-	public static void processOrder(int productCost, String orderStatus) {
+	public static double processOrder(int productCost, String orderStatus) {
 		double shippingCost = 10.50;
 		double totalCost = 0.0;
 		if (orderStatus == "shipped") {
 			totalCost = productCost + shippingCost;
 			System.out.println("The total cost of your shipped order is $" + totalCost + ".");
+			return totalCost;
 		} else if (orderStatus == "cancelled") {
 			totalCost = 0.0;
 			System.out.println("The total cost of your cancelled order is now $0.");
-		} 
+			return 0.0;
+		} else {
+			return 0.0;
+		}
 	}
 
 	public static void main(String[] args) {
--- Episode10/FixedLogicError.java
+++ Episode10/FixedLogicError.java
@@ -1,21 +1,25 @@
 package Episode10;
 
 public class FixedLogicError {
-	public static void processOrder(int productCost, String orderStatus) {
+	public static double processOrder(int productCost, String orderStatus) {
 		// Added functionality for return orders as well as a catch-all else statement
 		double shippingCost = 10.50;
 		double totalCost = 0.0;
 		if (orderStatus == "shipped") {
 			totalCost = productCost + shippingCost;
 			System.out.println("The total cost of your shipped order is $" + totalCost + ".");
+			return totalCost;
 		} else if (orderStatus == "cancelled") {
 			totalCost = 0.0;
 			System.out.println("The total cost of your cancelled order is now $0.");
+			return 0.0;
 		} else if (orderStatus == "returned") {
 			totalCost = productCost - shippingCost;
 			System.out.println("The total cost of your returned order is $" + totalCost + ".");
+			return totalCost;
 		} else {
 			System.out.println("Unsupported order status.");
+			return 0.0;
 		}
 	}
 
