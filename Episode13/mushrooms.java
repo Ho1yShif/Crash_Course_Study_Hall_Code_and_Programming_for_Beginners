f6b3e10d649e043f6a39c7352d3b882e975606eb
Ho1yShif <shifraisaacs@gmail.com>
2023-02-19 14:22:13 -0500

    Concat units to mushrooms program

--- Episode13/mushrooms.java
+++ Episode13/mushrooms.java
@@ -6,6 +6,6 @@ public class mushrooms {
 		double portabello = 15.0;
 
 		double chosenMushroomAmount = Math.max(shiitake, portabello);
-		System.out.println(chosenMushroomAmount); // Output: 15.0 for portabello
+		System.out.println(chosenMushroomAmount + " pounds"); // Output: 15.0 for portabello
 	}
 }
