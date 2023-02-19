08fa7c3e4405fce1565216558881499e0d5a264c
Ho1yShif <shifraisaacs@gmail.com>
2023-02-19 15:24:37 -0500

    Fix "portobello" spelling

--- Episode13/mushrooms.java
+++ Episode13/mushrooms.java
@@ -3,9 +3,9 @@ package Episode13;
 public class mushrooms {
 	public static void main(String[] args) {
 		double shiitake = 12.0;
-		double portabello = 15.0;
+		double portobello = 15.0;
 
-		double chosenMushroomAmount = Math.max(shiitake, portabello);
+		double chosenMushroomAmount = Math.max(shiitake, portobello);
 		System.out.println(chosenMushroomAmount + " pounds"); // Output: 15.0 for portabello
 	}
 }
