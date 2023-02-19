ab676cd63cfbe915febda4f406473c9eeae674a4
Ho1yShif <shifraisaacs@gmail.com>
2023-02-19 13:26:49 -0500

    Add salad pseudocode for ep 13

---
+++ Episode13/salad_pseudo.java
@@ -0,0 +1,15 @@
+package Episode13;
+
+public class salad_pseudo {
+	public static salad makeChickenSalad(String topping) {
+		int lettuce = 1;
+		int tomatoes = 1;
+		int onions = 1;
+		int croutons = 1;
+		System.out.println("You have a salad topped with " + topping);
+	}
+    
+    public static void main(String[] args) {
+		makeChickenSalad("chicken");
+	}
+}
