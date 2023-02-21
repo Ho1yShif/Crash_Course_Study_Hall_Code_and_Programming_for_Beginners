8bedf238a4920259efc3cdf96a0f613cf54edeb7
Shifra Isaacs <shifraisaacs@gmail.com>
2023-02-21 18:06:30 -0500

    Create side.java

---
+++ Episode13/side.java
@@ -0,0 +1,21 @@
+package Episode13;
+
+public class side {
+    public static void makeSandwich(String main_sandwich_type, String side_sandwich_type) {
+        String protein = "";
+        int sourdough = 1;
+        int lettuce = 1;
+        int tomatoes = 1;
+    
+        if (main_sandwich_type.equals("blt")) {
+          protein = "bacon";
+        } else if (main_sandwich_type.equals("chicken")) {
+          protein = "chicken";
+        }
+    
+        // recursively call makeSandwich to create a side sandwich
+        sandwich side_sandwich = makeSandwich(side_sandwich_type, "");
+    
+        System.out.println("You have a " + main_sandwich_type + " sandwich and a " side_sandwich_type + " side sandwich.");
+      }
+}
