7a808398c1503eefe4d29ca58b815b9a8a4a8eaf
Shifra Isaacs <shifraisaacs@gmail.com>
2023-02-22 16:19:33 -0500

    Update side.java

--- Episode13/side.java
+++ Episode13/side.java
@@ -1,25 +1,29 @@
 package Episode13;
 
 public class side {
-    public static void makeSandwich(String main_sandwich_type, String side_sandwich_type) {
+    public static sandwich makeSandwich(String main_sandwich_type, String side_sandwich_type) {
         String protein = "";
         int sourdough = 1;
         int lettuce = 1;
         int tomatoes = 1;
-    
+
         if (main_sandwich_type.equals("blt")) {
-          protein = "bacon";
+            protein = "bacon";
         } else if (main_sandwich_type.equals("chicken")) {
-          protein = "chicken";
+            protein = "chicken";
+        } else if (main_sandwich_type.equals("")) { // termination condition
+            return sandwich;
         }
-    
+
         // recursively call makeSandwich to create a side sandwich
         sandwich side_sandwich = makeSandwich(side_sandwich_type, "");
-    
-        System.out.println("You have a " + main_sandwich_type + " sandwich and a " side_sandwich_type + " side sandwich.");
-      }
-    
-    public static void main(String args[]) { 
+
+        System.out.println("You have a " + main_sandwich_type + " sandwich and a " + side_sandwich_type + " side sandwich.");
+
+        return side_sandwich;
+    }
+
+    public static void main(String args[]) {
         makeSandwich("blt", "chicken"); // our main and side sandwich orders
-  } 
+    }
 }
