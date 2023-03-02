a796c0d7eac138055708d78532384aa4c6ed275c
Ho1yShif <shifraisaacs@gmail.com>
2023-03-02 10:55:07 -0500

    Rename mushroom variables with units

--- Episode13/mushrooms.java
+++ Episode13/mushrooms.java
@@ -2,10 +2,10 @@ package Episode13;
 
 public class mushrooms {
 	public static void main(String[] args) {
-		double shiitake = 12.0;
-		double portobello = 15.0;
+		double shiitakePounds = 12.0;
+		double portobelloPounds = 15.0;
 
-		double chosenMushroomAmount = Math.max(shiitake, portobello);
+		double chosenMushroomAmount = Math.max(shiitakePounds, portobelloPounds);
 		System.out.println(chosenMushroomAmount + " pounds"); // Output: 15.0 for portabello
 	}
 }
