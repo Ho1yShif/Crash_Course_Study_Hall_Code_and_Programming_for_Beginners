520e068ee1c4958f349ed20aed1294e354166c5f
Ho1yShif <shifraisaacs@gmail.com>
2023-03-18 12:47:14 -0400

    Add weather code

---
+++ Episode08/Draft02/weather.java
@@ -0,0 +1,16 @@
+package Episode08.Draft02;
+
+public class weather {
+	public static void main(String[] args) {
+	Double reader_temp = 70.0; // degrees Celsius
+
+	// Conditional statements
+	if (reader_temp == 70.0) {
+		System.out.println("The weather outside is perfect!");
+	} else if (reader_temp  >= 95.0) {
+		System.out.println("Heat Warning: Remember to stay cool and stay hydrated!");
+	} else if (reader_temp  <= 32.0) {
+		System.out.println("Freeze Warning: Remember to wear lots of layers!");
+	}
+	} 
+}
