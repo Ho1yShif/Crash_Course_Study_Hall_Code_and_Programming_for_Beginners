ac660be71d69d8fe2973bcc2cde0fa1b295da999
Ho1yShif <shifraisaacs@gmail.com>
2023-03-18 13:52:54 -0400

    Fix typo in combined code

--- Episode08/Draft02/combined.java
+++ Episode08/Draft02/combined.java
@@ -4,7 +4,7 @@ public class combined {
 	public static void main(String[] args) {
 	Double reader_temp = 103.0; // degrees Fahrenheit
 
-	// Combined conditions operator
+	// Combined conditions
 	if ((reader_temp  >= 95.0 || reader_temp  <= 32.0) && reader_temp != 105 ) {
 		System.out.println("This weather meets some very specific criteria!");
 	}
