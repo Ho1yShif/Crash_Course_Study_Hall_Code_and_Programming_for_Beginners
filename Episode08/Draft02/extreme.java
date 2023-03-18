52c55ad2909a5b33a0ab65038e1566664bc6e091
Ho1yShif <shifraisaacs@gmail.com>
2023-03-18 13:29:17 -0400

    Add extreme check

---
+++ Episode08/Draft02/extreme.java
@@ -0,0 +1,12 @@
+package Episode08.Draft02;
+
+public class extreme {
+	public static void main(String[] args) {
+	Double reader_temp = 20.0; // degrees Fahrenheit
+
+	// OR operator
+	if (reader_temp  >= 95.0 || reader_temp  <= 32.0) {
+		System.out.println("The weather is extreme.");
+	}
+}
+}
