673c60b8e4fd7185620cc71a51df5d67807dd081
Ho1yShif <shifraisaacs@gmail.com>
2023-03-18 13:45:08 -0400

    Add cheese NOT code

---
+++ Episode08/Draft02/cheese_not.java
@@ -0,0 +1,12 @@
+package Episode08.Draft02;
+
+public class cheese_not {
+	public static void main(String[] args) {
+	Double reader_temp = 42.0; // degrees Fahrenheit
+
+	// Conditional statements
+	if (!(reader_temp == 40.0)) {
+		System.out.println("SAVE THE CHEESE!");
+	} 
+}
+}
