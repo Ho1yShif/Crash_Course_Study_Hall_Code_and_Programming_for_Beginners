699e272f06a95469b7c1c686b3c917ffe11f09b7
Ho1yShif <shifraisaacs@gmail.com>
2023-03-18 13:25:47 -0400

    Rename to vaccine warning

--- Episode08/Draft02/vaccine.java
+++ Episode08/Draft02/vaccine.java
@@ -1,12 +1,12 @@
 package Episode08.Draft02;
 
-public class LogicalAnd {
+public class vaccine {
 	public static void main(String[] args) {
 	Double reader_temp = -10.0; // degrees Fahrenheit
 
 	// AND operator
 	if (reader_temp >= -13.0 && reader_temp <= 5.0) {
-		System.out.println("The vaccines are safe");
+		System.out.println("The vaccines are safe.");
 	} else {
 		System.out.println("Vaccine safety warning: Get the temperature back to a controlled range!");
 	}
