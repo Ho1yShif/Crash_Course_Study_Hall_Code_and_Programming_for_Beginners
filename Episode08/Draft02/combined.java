c812a44a5bbfb6d2f13ab3fe96e58aa97612a34e
Ho1yShif <shifraisaacs@gmail.com>
2023-03-22 15:32:30 -0400

    Changing comparison mode in combined

--- Episode08/Draft02/combined.java
+++ Episode08/Draft02/combined.java
@@ -5,7 +5,7 @@ public class combined {
 	Double temperature = 103.0; // degrees Fahrenheit
 
 	// Combined conditions
-	if ((temperature  >= 95.0 || temperature  <= 32.0) && !(temperature < -130 && temperature > 140)) {
+	if ((temperature  >= 95.0 || temperature  <= 32.0) && !(temperature < -130 || temperature > 140)) {
 		System.out.println("This weather meets some very specific criteria!");
 	}
 }
