d0342a26d6f0d345741724f9e130e16712a258a2
Ho1yShif <shifraisaacs@gmail.com>
2023-04-13 10:53:24 -0400

    Fix runtime exception example

--- Episode10/FixedRuntimeException.java
+++ Episode10/FixedRuntimeException.java
@@ -2,8 +2,8 @@ package Episode10;
 
 public class FixedRuntimeException {
 	public static void main(String[] args) {
-		String name = "Bob";
-		char c = name.charAt(2);
-		System.out.print(c);
+		String word = "range";
+		char last_char = word.charAt(4);
+		System.out.print(last_char);
 	}
 }
--- Episode10/RuntimeException.java
+++ Episode10/RuntimeException.java
@@ -2,8 +2,8 @@ package Episode10;
 
 public class RuntimeException {
 	public static void main(String[] args) {
-		String name = "Bob";
-		char c = name.charAt(3);
-		System.out.print(c);
+		String word = "range";
+		char last_char = word.charAt(5);
+		System.out.print(last_char);
 	}
 }
