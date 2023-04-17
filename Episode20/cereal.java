e7e1127465cc9ad883a40249b64d6d2c51d9d07b
Ho1yShif <shifraisaacs@gmail.com>
2023-04-17 17:11:13 -0400

    Add cereal comparison to Ep20

---
+++ Episode20/cereal.java
@@ -0,0 +1,23 @@
+package Episode20;
+
+public class cereal {
+	
+	public static void main(String[] args) {
+		String boxOfCereal = new String("cereal");
+		String actualCereal = new String("cereal");
+		System.out.println(boxOfCereal == actualCereal);
+		System.out.println(boxOfCereal.equals(actualCereal));
+
+	if (boxOfCereal == actualCereal) {
+		System.out.println("boxOfCereal and actualCereal point to the same memory location");
+	} else {
+		System.out.println("boxOfCereal and actualCereal point to different memory locations");
+	}
+
+	if (boxOfCereal.equals(actualCereal)) {
+		System.out.println("boxOfCereal and actualCereal have the same content");
+	} else {
+		System.out.println("boxOfCereal and actualCereal have different content");
+	}
+}
+}
