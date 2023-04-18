d4bba2e8f460e74904e75d4ceb0097d06e10cfe1
Ho1yShif <shifraisaacs@gmail.com>
2023-04-17 22:16:22 -0400

    Add efficient cereal code

---
+++ Episode20/efficient_cereal.java
@@ -0,0 +1,11 @@
+package Episode20;
+
+public class efficient_cereal {
+	
+	public static void main(String[] args) {
+		String boxOfCereal = "cereal";
+		String actualCereal = boxOfCereal;
+		System.out.println(boxOfCereal == actualCereal);
+		System.out.println(boxOfCereal.equals(actualCereal));
+}
+}
