16f39e3d7466fb225ab2686560c96fb0c18eb86c
Ho1yShif <shifraisaacs@gmail.com>
2023-04-18 01:21:57 -0400

    Add primitives_copied

---
+++ Episode20/primitives_copied.java
@@ -0,0 +1,10 @@
+package Episode20;
+
+public class primitives {
+	
+	public static void main(String[] args) {
+		int avatarElements = 4;
+		int boxesOfCTS = avatarElements;
+		System.out.println(avatarElements == boxesOfCTS);
+	}
+}
