183da16505a2ca14993051c1a1fe20243a359318
Ho1yShif <shifraisaacs@gmail.com>
2023-04-17 20:12:53 -0400

    Fix indentation

--- Episode20/cereal.java
+++ Episode20/cereal.java
@@ -8,16 +8,16 @@ public class cereal {
 		System.out.println(boxOfCereal == actualCereal);
 		System.out.println(boxOfCereal.equals(actualCereal));
 
-	if (boxOfCereal == actualCereal) {
-		System.out.println("boxOfCereal and actualCereal point to the same memory location");
-	} else {
-		System.out.println("boxOfCereal and actualCereal point to different memory locations");
-	}
+		if (boxOfCereal == actualCereal) {
+			System.out.println("boxOfCereal and actualCereal point to the same memory location");
+		} else {
+			System.out.println("boxOfCereal and actualCereal point to different memory locations");
+		}
 
-	if (boxOfCereal.equals(actualCereal)) {
-		System.out.println("boxOfCereal and actualCereal have the same content");
-	} else {
-		System.out.println("boxOfCereal and actualCereal have different content");
-	}
+		if (boxOfCereal.equals(actualCereal)) {
+			System.out.println("boxOfCereal and actualCereal have the same content");
+		} else {
+			System.out.println("boxOfCereal and actualCereal have different content");
+		}
 }
 }
