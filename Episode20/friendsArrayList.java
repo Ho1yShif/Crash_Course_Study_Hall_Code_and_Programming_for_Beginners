351a916003f8ae58bd3e4d2e0faebaec4b9d46cd
Ho1yShif <shifraisaacs@gmail.com>
2023-04-18 21:51:26 -0400

    Add friendsArrayList

---
+++ Episode20/friendsArrayList.java
@@ -0,0 +1,15 @@
+package Episode20;
+
+import java.util.ArrayList;
+
+public class friendsArrayList {
+	
+	public static void main(String[] args) {
+		ArrayList<String> friends = new ArrayList<String>();
+		friends.add("Taha");
+		friends.add("Melissa");
+		friends.add("Hank");
+		friends.add("John");
+		friends.add("Tom");
+	}
+}
