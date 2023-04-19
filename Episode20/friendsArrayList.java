15371cb341409f447dd63f107e239bce27b931fb
Ho1yShif <shifraisaacs@gmail.com>
2023-04-18 21:54:16 -0400

    Add print statements to friendsArrayList

--- Episode20/friendsArrayList.java
+++ Episode20/friendsArrayList.java
@@ -6,10 +6,12 @@ public class friendsArrayList {
 	
 	public static void main(String[] args) {
 		ArrayList<String> friends = new ArrayList<String>();
+		System.out.println("Before: " + friends.size()); // Before: 0
 		friends.add("Taha");
 		friends.add("Melissa");
 		friends.add("Hank");
 		friends.add("John");
 		friends.add("Tom");
+		System.out.println("After: " + friends.size()); // After: 5
 	}
 }
