8263f4b69e31d4002f0bd8cae8529a84871a7d2b
Ho1yShif <shifraisaacs@gmail.com>
2023-04-25 20:47:18 -0400

    Update scores code to intake a list of lists

--- Episode22/scores.java
+++ Episode22/scores.java
@@ -3,17 +3,22 @@ package Episode22;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class scores {
 	public static void main(String[] args) {
 		try {
 			// Open the file using FileInputStream
-			FileInputStream inputStream = new FileInputStream("/Users/shifra.isaacs/Documents/GH/CC_SH_PoP/Episode22/names_and_scores.txt");
-
+			FileInputStream inputStream = new FileInputStream("student_scores.txt");
+	
 			// Create a Scanner to read the file
 			Scanner scanner = new Scanner(inputStream);
-
-			// Loop through the lines and print the test scores for each student
+	
+			// Initialize ArrayLists to store the names and scores
+			ArrayList<String> names = new ArrayList<>();
+			ArrayList<ArrayList<Integer>> scores = new ArrayList<>();
+	
+			// Loop through the lines and add the names and scores to the ArrayLists
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
 				String[] parts = line.split(",");
@@ -21,16 +26,33 @@ public class scores {
 				int score1 = Integer.parseInt(parts[1].trim());
 				int score2 = Integer.parseInt(parts[2].trim());
 				int score3 = Integer.parseInt(parts[3].trim());
-				System.out.println(studentName + ": " + score1 + ", " + score2 + ", " + score3);
+	
+				// Add the name to the names ArrayList
+				names.add(studentName);
+	
+				// Create a new ArrayList to store the scores for this student
+				ArrayList<Integer> studentScores = new ArrayList<>();
+				studentScores.add(score1);
+				studentScores.add(score2);
+				studentScores.add(score3);
+	
+				// Add the scores ArrayList to the scores ArrayList of ArrayLists
+				scores.add(studentScores);
 			}
-
+	
+			// Print the contents of the names and scores ArrayLists
+			for (int i = 0; i < names.size(); i++) {
+				String studentName = names.get(i);
+				ArrayList<Integer> studentScores = scores.get(i);
+				System.out.println(studentName + ": " + studentScores.get(0) + ", " + studentScores.get(1) + ", " + studentScores.get(2));
+			}
+	
 			// Close the FileInputStream and Scanner
 			inputStream.close();
 			scanner.close();
-
+	
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
+	}	
 }
-
