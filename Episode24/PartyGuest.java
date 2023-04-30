c0b56e001e978225afc464aece2cc5c2b7ae8db7
Ho1yShif <shifraisaacs@gmail.com>
2023-04-30 15:00:53 -0400

    Convert to tabs

--- Episode24/PartyGuest.java
+++ Episode24/PartyGuest.java
@@ -1,23 +1,23 @@
 package Episode24;
 
 public class PartyGuest {
-    String name;
-    int age;
-    String favoriteDrink;
-    boolean allergicToPeanuts;
+	String name;
+	int age;
+	String favoriteDrink;
+	boolean allergicToPeanuts;
 
-    public PartyGuest(String name, int age, String favoriteDrink, boolean allergicToPeanuts) {
-        this.name = name;
-        this.age = age;
-        this.favoriteDrink = favoriteDrink;
-        this.allergicToPeanuts = allergicToPeanuts;
-    }
+	public PartyGuest(String name, int age, String favoriteDrink, boolean allergicToPeanuts) {
+		this.name = name;
+		this.age = age;
+		this.favoriteDrink = favoriteDrink;
+		this.allergicToPeanuts = allergicToPeanuts;
+	}
 
-    public static void main(String[] args) {
-        PartyGuest zack = new PartyGuest("Zack", 25, "Ginger Ale", false);
-        System.out.println("Name: " + zack.name);
-        System.out.println("Age: " + zack.age);
-        System.out.println("Favorite Drink: " + zack.favoriteDrink);
-        System.out.println("Allergic to peanuts: " + zack.allergicToPeanuts);
-    }
+	public static void main(String[] args) {
+		PartyGuest zack = new PartyGuest("Zack", 25, "Ginger Ale", false);
+		System.out.println("Name: " + zack.name);
+		System.out.println("Age: " + zack.age);
+		System.out.println("Favorite Drink: " + zack.favoriteDrink);
+		System.out.println("Allergic to peanuts: " + zack.allergicToPeanuts);
+	}
 }
