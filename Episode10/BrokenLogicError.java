efcc2d1f43f1ee546849328900976c7ecff46ded
Ho1yShif <shifraisaacs@gmail.com>
2023-05-03 14:39:37 -0400

    Update logic error to use compareTo method

--- Episode10/BrokenLogicError.java
+++ Episode10/BrokenLogicError.java
@@ -5,11 +5,11 @@ public class BrokenLogicError {
 	public static double processOrder(int productCost, String orderStatus) {
 		double shippingCost = 10.50;
 		double totalCost = 0.0;
-		if (orderStatus == "shipped") {
+		if (orderStatus.compareTo("shipped") == 0) {
 			totalCost = productCost + shippingCost;
 			System.out.println("The total cost of your shipped order is $" + totalCost + ".");
 			return totalCost;
-		} else if (orderStatus == "cancelled") {
+		} else if (orderStatus.compareTo("cancelled") == 0) {
 			totalCost = 0.0;
 			System.out.println("The total cost of your cancelled order is now $0.");
 			return 0.0;
