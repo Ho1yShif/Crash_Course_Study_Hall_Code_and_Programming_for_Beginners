8a28a679c606cb5a5d13858b2e33b93f86277fbf
Ho1yShif <shifraisaacs@gmail.com>
2023-05-12 10:58:33 -0400

    Add monster class

---
+++ Episode28/Monster.java
@@ -0,0 +1,37 @@
+package Episode28;
+
+public class Monster {
+	private String name;
+	private int currentHealth;
+	private Weapon weapon;
+
+	public Monster(String name, int currentHealth, Weapon weapon) {
+		this.name = name;
+		this.currentHealth = currentHealth;
+		this.weapon = weapon;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+
+	public Weapon getWeapon() {
+		return weapon;
+	}
+
+	public void attack(Monster other) {
+		int damage = this.weapon.getMaxDamage();
+		other.takeDamage(damage);
+		System.out.println(this.name + " attacks " + other.getName() + " with " + this.weapon.getName() + " for " + damage + " damage.");
+	}
+
+	public void takeDamage(int damage) {
+		this.currentHealth -= damage;
+		System.out.println(this.name + " takes " + damage + " damage. Current health: " + this.currentHealth);
+	}
+}
+
