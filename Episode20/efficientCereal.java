b04d09a407fe5158811c434861f7123c43c42b30
Ho1yShif <shifraisaacs@gmail.com>
2023-07-18 16:28:32 -0700

    Update vars for efficient cereal code

--- Episode20/efficientCereal.java
+++ Episode20/efficientCereal.java
@@ -3,9 +3,9 @@ package Episode20;
 public class efficientCereal {
 	
 	public static void main(String[] args) {
-		String boxOfCereal = "cereal";
-		String actualCereal = boxOfCereal;
-		System.out.println(boxOfCereal == actualCereal);
-		System.out.println(boxOfCereal.equals(actualCereal));
+		String CocoaPuffs = "cereal";
+		String FrootLoops = CocoaPuffs;
+		System.out.println(CocoaPuffs == FrootLoops);
+		System.out.println(CocoaPuffs.equals(FrootLoops));
 	}
 }
